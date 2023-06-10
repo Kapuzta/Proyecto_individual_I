08ae205a361f9b483e921be163237c4d34be50c8
Israel Nieves <103685051+Kapuzta@users.noreply.github.com>
2023-06-09 19:59:10 -0500

    Add files via upload

---
+++ limpieza de datos.py
@@ -0,0 +1,63 @@
+import pandas as pd
+import numpy as np
+import json
+import re
+
+movies = pd.read_csv('movies_dataset.csv', low_memory=False)
+credits = pd.read_csv('credits.csv', low_memory=False)
+
+# es una clase para extraer el valor name de las columnas que contienen listas o diccionarios
+class desanidar:
+    @staticmethod
+    def convertir_a_str(valor):
+        if isinstance(valor, (list, dict)):
+            return json.dumps(valor)
+        return str(valor)
+    @staticmethod
+    def extraer_nombres(valor):
+        pattern = r"'name': '([^']*)'"
+        coincidencias = re.findall(pattern, valor)
+        if len(coincidencias) > 0:
+            nombre = coincidencias[0]
+            return nombre
+        else:
+            return None
+        
+movies['belongs_to_collection'] = movies['belongs_to_collection'].apply(desanidar.convertir_a_str).apply(desanidar.extraer_nombres)
+movies['genres'] = movies['genres'].apply(desanidar.convertir_a_str).apply(desanidar.extraer_nombres)
+movies['production_companies'] = movies['production_companies'].apply(desanidar.convertir_a_str).apply(desanidar.extraer_nombres)
+movies['production_countries'] = movies['production_countries'].apply(desanidar.convertir_a_str).apply(desanidar.extraer_nombres)
+movies['spoken_languages'] = movies['spoken_languages'].apply(desanidar.convertir_a_str).apply(desanidar.extraer_nombres)
+credits['cast'] = credits['cast'].apply(desanidar.convertir_a_str).apply(desanidar.extraer_nombres)
+credits['crew'] = credits['crew'].apply(desanidar.convertir_a_str).apply(desanidar.extraer_nombres)
+
+# rellenar con 0 en valores nulos de los campos revenue y budget
+movies[['budget', 'revenue']] = movies[['budget', 'revenue']].fillna(0)
+
+# los valores de budget a numerico ya que es objeto
+movies['budget'] = pd.to_numeric(movies['budget'], errors='coerce', downcast='integer')
+movies['budget'] = movies['budget'].fillna(0)
+
+# pasar a enteros
+movies[['budget', 'revenue']] = movies[['budget', 'revenue']].astype(np.int64)
+
+#cambiar el formato de las fechas
+movies['release_date'] = pd.to_datetime(movies['release_date'], format='%Y-%m-%d', errors='coerce')
+
+# eliminar campos nulos de release_date
+movies['release_date'] = movies['release_date'].fillna(np.nan)
+movies = movies.dropna(subset=['release_date'])
+
+# crear nueva columna con el año de estreno
+movies['release_year'] = movies['release_date'].dt.year
+
+# una funcion anonima para crear la columna return
+movies['return'] = movies.apply(lambda row: row['revenue'] / row['budget'] if pd.notnull(row['revenue']) and pd.notnull(row['budget']) and row['budget'] != 0 else 0, axis=1)
+
+# eliminar columnas irrelevantes
+eliminar_cols = movies[['video', 'imdb_id', 'adult', 'original_title', 'poster_path', 'homepage']]
+movies = movies.drop(eliminar_cols, axis=1)
+
+movies['id'] = movies['id'].astype(np.int64)
+movies_credits_dataset = pd.merge(movies, credits, on='id')
+movies_credits_dataset.to_csv('movies_credits_dataset.csv', index=False)
---
+++ main.py
@@ -0,0 +1,128 @@
+from fastapi import FastAPI
+import pandas as pd
+
+app = FastAPI()
+
+movies = pd.read_csv('movies_credits_dataset.csv', parse_dates=['release_date'], low_memory=False)
+
+#diccionario para que los meses sean en español
+meses_dict = {
+    'enero': 1,
+    'febrero': 2,
+    'marzo': 3,
+    'abril': 4,
+    'mayo': 5,
+    'junio': 6,
+    'julio': 7,
+    'agosto': 8,
+    'septiembre': 9,
+    'octubre': 10,
+    'noviembre': 11,
+    'diciembre': 12
+}
+
+#diccionario para que los dias sean en español
+dias_dict = {
+    'lunes': 0,
+    'martes': 1,
+    'miércoles': 2,
+    'jueves': 3,
+    'viernes': 4,
+    'sábado': 5,
+    'domingo': 6
+}
+
+# funcion 1
+@app.get('/cantidad_filmaciones_mes/{mes}')
+def cantidad_filmaciones_mes(mes: str):
+    mes_minus = mes.lower()
+    if mes_minus in meses_dict:
+        num_mes = meses_dict[mes_minus]
+        peliculas_mes = movies[movies['release_date'].dt.month == num_mes]
+        cantidad = len(peliculas_mes)
+        return {'mes':mes, 'cantidad':cantidad}
+    else:
+        return {}
+    
+# funcion 2
+@app.get('/cantidad_filmaciones_dia/{dia}')
+def cantidad_filmaciones_dia(dia: str):
+    dia_minus = dia.lower()
+    if dia_minus in dias_dict:
+        num_dia = dias_dict[dia_minus]
+        peliculas_dia = movies[movies['release_date'].dt.dayofweek == num_dia]
+        cantidad = len(peliculas_dia)
+        return {'dia':dia, 'cantidad':cantidad}
+    else:
+        return {}
+    
+# funcion 3
+@app.get('/score_titulo/{titulo}')
+def score_titulo(titulo: str):
+    pelicula = movies[movies['title'] == titulo]
+    if len(pelicula) > 0:
+        titulo_filmacion = pelicula['title'].iloc[0]
+        fecha_estreno = int(pelicula['release_year'].iloc[0])
+        score = round(pelicula['popularity'].iloc[0], 2)
+        return {'titulo':titulo_filmacion, 'anio':fecha_estreno, 'popularidad':score}
+    else:
+        return {}
+    
+# funcion 4
+@app.get('/votos_titulo/{titulo}')
+def votos_titulo(titulo: str):
+    pelicula = movies[movies['title'] == titulo]
+    if len(pelicula) > 0:
+        titulo_filmacion = pelicula['title'].iloc[0]
+        anio_estreno = pelicula['release_year'].iloc[0]
+        total_votos = pelicula['vote_count'].iloc[0].astype(int)
+        promedio_votos = pelicula['vote_average'].iloc[0]
+        
+        if total_votos >= 2000:
+            return {'titulo':titulo_filmacion, 'anio':anio_estreno, 'voto_total':total_votos, 'voto_promedio':promedio_votos}
+        else:
+            return {'La película '+(titulo)+' no cumple con el requisito de tener al menos 2000 valoraciones.'}
+    else:
+        return {}
+    
+# funcion 5
+@app.get('/get_actor/{nombre_actor}')
+def get_actor(nombre_actor):
+    peliculas_actor = movies[(movies['cast'] == nombre_actor) & (movies['crew'] != nombre_actor)]
+    cantidad_peliculas = len(peliculas_actor)
+    exito = round(peliculas_actor['return'].sum(),2)
+    promedio_retorno = round((exito / cantidad_peliculas),2)
+    
+    return {'actor':nombre_actor, 'cantidad_filmaciones':cantidad_peliculas, 'retorno_total':exito, 'retorno_promedio':promedio_retorno}
+
+# funcion 6
+@app.get('/get_director/{nombre_director}')
+def get_director(nombre_director):
+    peliculas_director = movies[movies['crew'] == nombre_director]
+    exito_director = round((peliculas_director['return'].sum()), 2)
+    
+    peliculas = []
+    
+    for _, pelicula in peliculas_director.iterrows():
+        titulo = pelicula['title']
+        anio_lanzamiento = pelicula['release_year']
+        retorno_individual = round((pelicula['return']), 2)
+        costo = pelicula['budget']
+        ganancia = pelicula['revenue']
+        
+        peliculas.append({
+            'titulo': titulo,
+            'fecha_lanzamiento': anio_lanzamiento,
+            'retorno_individual': retorno_individual,
+            'costo': costo,
+            'ganancia': ganancia
+        })
+        
+    return {'director':nombre_director, 'retorno_total_director':exito_director, 'peliculas':peliculas}
+
+# ML
+#@app.get('/recomendacion/{titulo}')
+#def recomendacion(titulo:str):
+#    '''Ingresas un nombre de pelicula y te recomienda las similares en una lista'''
+#    return {'lista recomendada': respuesta}
+
