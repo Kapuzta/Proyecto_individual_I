ee87be0886a437ea8ea0ffc40a014a0035afaade
Israel Nieves <103685051+Kapuzta@users.noreply.github.com>
2023-06-09 21:15:28 -0500

    Update main.py

--- main.py
+++ main.py
@@ -78,7 +78,7 @@ def votos_titulo(titulo: str):
     if len(pelicula) > 0:
         titulo_filmacion = pelicula['title'].iloc[0]
         anio_estreno = pelicula['release_year'].iloc[0]
-        total_votos = pelicula['vote_count'].iloc[0].astype(np.int64)
+        total_votos = pelicula['vote_count'].iloc[0]
         promedio_votos = pelicula['vote_average'].iloc[0]
         
         if total_votos >= 2000:
